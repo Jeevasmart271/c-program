1af26c7af1440e3ee11ac6f7daba05b4ce02f18e
Jeeva K <40887075+Jeevasmart271@users.noreply.github.com>
2018-07-10 11:54:19 -0700

    Create Beg-set-2-5.c

---
+++ Beg-set-2-5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+clrscr();
+scanf("%d\t%d",&a,&b);
+for(i=a+1;i<j;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+else
+{
+printf(" ");
+}
+getch();
+}
