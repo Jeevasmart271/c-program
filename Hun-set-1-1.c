724ae02560a45457b803d777ba421441c7d4d313
Jeeva K <40887075+Jeevasmart271@users.noreply.github.com>
2018-07-10 12:12:30 -0700

    Create Hun-set-1-1.c

---
+++ Hun-set-1-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i,j;
+clrscr();
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+b[j]=a[i];
+}
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]==b[j])
+{
+printf("%d",a[i]);
+}
+}
+}
+getch();
+}
