851ff600644ec848f46a391e49cd28a9db74f55c
Jeeva K <40887075+Jeevasmart271@users.noreply.github.com>
2018-07-14 11:47:59 -0700

    Create Ply-set-1-9.c

---
+++ Ply-set-1-9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i,c=0;
+clrscr();
+scanf("%d %d",&a,&b);
+for(i=r;i<=c;i++)
+{
+if(i==2||i==3||i==5||i==7)
+{
+c++;
+}
+}
+printf("%d",c);
+getch();
+}
