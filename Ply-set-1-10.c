b6620a2f8f52ff155445cd1ee53c6ded616b8f59
Jeeva K <40887075+Jeevasmart271@users.noreply.github.com>
2018-07-14 22:55:36 -0700

    Update Ply-set-1-10.c

--- Ply-set-1-10.c
+++ Ply-set-1-10.c
@@ -3,20 +3,20 @@
 void main()
 {
 char a[3],b[3];
-int i,j,p=0,q=0;
+int i,j,c=0,d=0;
 clrscr();
 scanf("%s %s",a,b);
 for(i=1;i<=3;i++)
 {
-p=a[i+1]-a[i];
+c=a[i+1]-a[i];
 }
 for(j=1;j<=3;j++)
 {
-q=b[j+1]-b[j];
+d=b[j+1]-b[j];
 }
-if(p==q)
-printf("YES");
+if(c==d)
+printf("Yes");
 else
-printf("NO");
+printf("No");
 getch();
 }
