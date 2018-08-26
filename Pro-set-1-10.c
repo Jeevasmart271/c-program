a8b9ace9954457c2a9851a3462f6fecfd738d59d
Jeeva K <40887075+Jeevasmart271@users.noreply.github.com>
2018-08-26 14:02:23 +0530

    Create Pro-set-1-10.c

---
+++ Pro-set-1-10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+   int a[100],n,i,j,d,s=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&a[i]);
+   }
+   for(i=n-1;i>=0;i--)
+   {
+       for(j=0;j<i;j++)
+       {
+           d=a[i]-a[j];
+           s=s+d;
+       }
+   }
+   printf("%d",s);
+}
